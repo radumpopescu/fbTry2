29274ea983cd9d2659ccb4e9e167dcad356369c8
Radu Popescu <radumihailpopescu@gmail.com>
2017-03-11 14:46:02 +0200

    Added Twig extension and Bootstrap Bower

--- app.php
+++ app.php
@@ -5,6 +5,18 @@ require "model/Model.php";
 require "model/User.php";
 require "model/Post.php";
 
+
+echo $twig->render('index.html.twig', array('name' => 'Fabien'));
+
+
+//$loader = new Twig_Loader_Array(array(
+//    'index' => 'Hello {{ name }}!',
+//));
+//$twig = new Twig_Environment($loader);
+//
+//echo $twig->render('index', array('name' => 'Fabien'));
+
+//var_dump("A");
 //$u = new User(1);
 //$u->setName("Will Smith");
 //$u->save();
--- config.php
+++ config.php
@@ -16,3 +16,9 @@ try{
     new \Pixie\Connection('mysql', $dbConfig, 'QB');
 }
 catch(Exception $e){}
+
+
+$loader = new Twig_Loader_Filesystem('./views');
+$twig = new Twig_Environment($loader, array(
+    'cache' => './cache',
+));
